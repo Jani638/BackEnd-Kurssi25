89853cd013281c41e0e57ac94f0a3861b7bca77a
bhq638@myy.haaga-helia.fi <bhq638@myy.haaga-helia.fi>
2025-08-31 16:30:50 +0300

    Viikko2tehtavat

---
+++ viikko2tehtavat/viikko2tehtava2/viikko2tehtava2/src/main/java/com/example/viikko2tehtava2/Viikko2tehtava2Application.java
@@ -0,0 +1,13 @@
+package com.example.viikko2tehtava2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Viikko2tehtava2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Viikko2tehtava2Application.class, args);
+    }
+
+}
---
+++ viikko2tehtavat/viikko2tehtava2/viikko2tehtava2/src/test/java/com/example/viikko2tehtava2/Viikko2tehtava2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.viikko2tehtava2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Viikko2tehtava2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
